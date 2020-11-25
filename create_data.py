52c574202d5f0922b2db20e941e98f83ea3b095d
twinstae <rabolution@gmail.com>
2020-11-25 23:37:46 +0900

    tragedy 상속으로 분리

--- create_data.py
+++ create_data.py
@@ -10,28 +10,40 @@ class PerfCalculator:
         self.perf = a_perf
         self.play = a_play
 
-    def amount_for(self):
-        play = self.perf['play']
+    def amount(self):
         result = 0
-        if play['type'] == "tragedy":
-            result = 40000
-            if self.perf["audience"] > 30:
-                result += 1000 * (self.perf["audience"] - 30)
-        elif play['type'] == "comedy":
+        if self.play['type'] == "tragedy":
+            print("error!!!!")
+            raise Exception
+        elif self.play['type'] == "comedy":
             result = 30000
             if self.perf["audience"] > 20:
                 result += 10000 + 500 * (self.perf["audience"] - 20)
         return result
 
-    def volume_credit_for(self):
+    def volume_credit(self):
         result = max(self.perf["audience"] - 30, 0)
         if "comedy" == self.perf['play']['type']:
             result += self.perf['audience'] // 5
         return result
 
 
+class TragedyCalculator(PerfCalculator):
+    def amount(self):
+        result = 40000
+        if self.perf["audience"] > 30:
+            result += 1000 * (self.perf["audience"] - 30)
+        return result
+
+
 def play_for(perf):
     return play_data[perf["playID"]]
+
+
+def create_calc(perf):
+    if play_for(perf)['type'] == 'tragedy':
+        return TragedyCalculator(perf, play_for(perf))
+    return PerfCalculator(perf, play_for(perf))
 
 
 def create_data(invoice):
@@ -47,10 +59,10 @@ def create_data(invoice):
     }
 
     for perf in data['performances']:  # enrich perf
-        calc = PerfCalculator(perf, play_for(perf))
+        calc = create_calc(perf)
         perf['play'] = play_for(perf)
-        perf['amount'] = calc.amount_for()
-        perf['credit'] = calc.volume_credit_for()
+        perf['amount'] = calc.amount()
+        perf['credit'] = calc.volume_credit()
 
     data["total_amount"] = total_amount()
     data["total_volume_credits"] = total_volume_credits()
