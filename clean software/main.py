ec91ef02f5b1c03592307c93f337b2ff558589fe
twinstae <rabolution@gmail.com>
2020-11-26 20:40:23 +0900

    클래스 추출

--- clean software/main.py
+++ clean software/main.py
@@ -1,43 +1,41 @@
-def generate_primes(max_value):
-    if max_value >= 2:
-        f = init(max_value)  # 파이써닉한 꼼수
-        f = find_prime(f)
-        primes = extract_prime(f)
-        return primes
-    return []
+class PrimeCalculator:
+    f = []
 
+    def init_f(self, max_value):
+        self.f = [True] * (max_value + 1)
 
-def find_prime(f):
-    for i in range(2, int(len(f) ** 0.5)):
-        if f[i]:
-            for j in range(2 * i, len(f), i):
-                f[j] = False
-    return f
+    def generate_primes(self, max_value):
+        if max_value + 1 > 2:
+            self.init_f(max_value)
+            self.find_prime()
+            return self.extract_prime()
+        return []
 
+    def find_prime(self):
+        self.f[0] = False
+        self.f[1] = False
+        for i in range(2, int(len(self.f) ** 0.5)):
+            if self.f[i]:
+                for j in range(2 * i, len(self.f), i):
+                    self.f[j] = False
 
-def init(max_value):
-    f = [True] * (max_value + 1)
-    f[0] = False
-    f[1] = False
-    return f
-
-
-def extract_prime(f):
-    return [i for i in range(len(f)) if f[i]]
+    def extract_prime(self):
+        return [i for i, is_prime in enumerate(self.f) if is_prime]
 
 
 if __name__ == '__main__':
-    if not generate_primes(0):
+    calc = PrimeCalculator()
+    if not calc.generate_primes(0):
         print("테스트 통과, 0이하의 소수는 없다")
 
-    if [2] == generate_primes(2):
+    if [2] == calc.generate_primes(2):
         print("테스트 통과. 2 이하의 소수는 [2] 뿐이다.")
     else:
-        print("테스트 실패")
+        print("테스트 실패", calc.generate_primes(2))
 
     prime_list = [2, 3, 5, 7, 11, 13, 17, 19]
     passed = True
-    for a, b in zip(prime_list, generate_primes(20)):
+    for a, b in zip(prime_list, calc.generate_primes(20)):
         if a != b:
             print("test 실패", a, "=/=", b)
             passed = False
