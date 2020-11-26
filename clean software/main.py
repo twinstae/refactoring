98d19b3bb231b7d07010d777378356bb173dcc5e
twinstae <rabolution@gmail.com>
2020-11-26 20:17:55 +0900

    테스트 보강. 부등호 에러 잡음.

--- clean software/main.py
+++ clean software/main.py
@@ -1,16 +1,16 @@
 def generate_primes(max_value):
-    if max_value > 2:
-        s = max_value +1
-        f = [True] * s # 파이써닉한 꼼수
+    if max_value >= 2:
+        s = max_value + 1
+        f = [True] * s  # 파이써닉한 꼼수
         f[0] = False
         f[1] = False
 
-        for i in range(2,int(s**0.5)):
+        for i in range(2, int(s**0.5)):
             if f[i]:
                 for j in range(2*i, s, i):
                     f[j] = False
 
-        count = sum(f) # 파이써닉한 꼼수2. 사실 리스트는 동적 배열이라 필요도 없다.
+        count = sum(f)  # 파이써닉한 꼼수2. 사실 리스트는 동적 배열이라 필요도 없다.
 
         primes = [i for i in range(s) if f[i]]
         return primes
@@ -18,6 +18,14 @@ def generate_primes(max_value):
 
 
 if __name__ == '__main__':
+    if not generate_primes(0):
+        print("테스트 통과, 0이하의 소수는 없다")
+
+    if [2] == generate_primes(2):
+        print("테스트 통과. 2 이하의 소수는 [2] 뿐이다.")
+    else:
+        print("테스트 실패")
+
     prime_list = [2, 3, 5, 7, 11, 13, 17, 19]
     for a, b in zip(prime_list, generate_primes(20)):
         if a == b:
