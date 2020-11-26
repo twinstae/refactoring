b23d838c3ddfcf156a996104ae1b4cf44d1f52e0
twinstae <rabolution@gmail.com>
2020-11-26 20:13:09 +0900

    책에 나온대로 구현 + 약간의 파이써닉한 꼼수들

--- clean software/main.py
+++ clean software/main.py
@@ -1,5 +1,20 @@
 def generate_primes(max_value):
-    return [2, 3, 5, 7, 11, 13, 17, 19]
+    if max_value > 2:
+        s = max_value +1
+        f = [True] * s # 파이써닉한 꼼수
+        f[0] = False
+        f[1] = False
+
+        for i in range(2,int(s**0.5)):
+            if f[i]:
+                for j in range(2*i, s, i):
+                    f[j] = False
+
+        count = sum(f) # 파이써닉한 꼼수2. 사실 리스트는 동적 배열이라 필요도 없다.
+
+        primes = [i for i in range(s) if f[i]]
+        return primes
+    return []
 
 
 if __name__ == '__main__':
