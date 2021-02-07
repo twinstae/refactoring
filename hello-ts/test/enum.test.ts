31f1f6a88308821fe11974c1b023cb27fc2578a7
twinstae <rabolution@gmail.com>
2021-02-07 11:49:37 +0900

    count Color Enum

--- hello-ts/test/enum.test.ts
+++ hello-ts/test/enum.test.ts
@@ -19,4 +19,38 @@ describe('basic types', () => {
         let colorName: string = Color[2];
         expect(colorName).toBe('Green');
     })
+    test('Enum count', () => {
+        enum Color {
+            red,
+            green,
+            blue
+        }
+
+        const colorList: Color[] = [
+            Color.red, Color.green, Color.blue,
+            Color.red, Color.blue, Color.red
+        ];
+
+        
+        const colorKeys: string[] = Object.keys(Color)
+            .filter(key=>isNaN(parseInt(key)));
+            
+        function count<T>(list: T[] , expected: T): number {
+            return list.filter(v=>v==expected).length
+        };
+        
+        const initial: { [key: string]: number} = {};
+
+        const colorsCount = colorKeys.reduce((result, color)=>({
+            ...result,
+            [color]: count(colorList, Color[color])
+        }), initial);
+
+        console.log(Color);
+        console.log(colorsCount);
+        expect(colorsCount.red).toBe(3);
+        expect(colorsCount.green).toBe(1);
+        expect(colorsCount.blue).toBe(2);
+    })
 })
+
