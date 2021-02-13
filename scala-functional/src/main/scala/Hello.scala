e0d510da2814224afc069416919214e6d41cd171
twinstae <rabolution@gmail.com>
2021-02-13 19:21:21 +0900

    고차함수 목록2.2 factorial, 연습문제 2.1 fibonacci

--- scala-functional/src/main/scala/Hello.scala
+++ scala-functional/src/main/scala/Hello.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Hello {
   def abs(n: Int): Int =
     if (n < 0) -n
@@ -8,7 +10,37 @@ object Hello {
     msg.format(x, abs(x))
   }
 
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n * acc)
+
+    go(n, 1)
+  }
+
+  private def formatFactorial(x: Int) =
+    "The factorial of %d is %d".format(x, factorial(x))
+
+  def fib(n: Int): Int = {
+    def go(now: Int, a: Int, b: Int): Int =
+      if (n == now) a
+      else go(now+1, b, a+b)
+
+    go(1, 0, 1)
+  }
+
+  private def formatFib(x: Int): String =
+    "The %dth fibonacci number is %d".format(x, fib(x))
+
   def main(args: Array[String]): Unit ={
     println(formatAbs(-42))
+    println()
+    println(formatFactorial(5))
+    println()
+    println(formatFib(3))
+    println(formatFib(4))
+    println(formatFib(5))
+    println(formatFib(6))
   }
 }
