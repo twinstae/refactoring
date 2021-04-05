68ea6509ee0a73d1115135f7a1ce7fbc2b8b6648
twinstae <rabolution@gmail.com>
2021-04-05 22:16:55 +0900

    foreign key, join author (user)

--- py-db/aiosqlite_test.py
+++ py-db/aiosqlite_test.py
@@ -3,7 +3,10 @@ from sqlite3 import Row
 from typing import List, Tuple, Coroutine, Any, Iterable, Optional
 from unittest import TestCase
 
+TEST_USER_NAME = "test"
+
 ARTICLES = "ARTICLES"
+USER = "USER"
 
 
 class Sqlite3Test(TestCase):
@@ -11,7 +14,17 @@ class Sqlite3Test(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.con.execute(f'''CREATE TABLE {ARTICLES} (title text, body text);''')
+        cls.con.execute(f'''CREATE TABLE {ARTICLES} (
+            id integer primary key,
+            title text,
+            body text,
+            FOREIGN KEY(author) REFERENCES USER(id)
+        );''')
+        cls.con.execute(f'''CREATE TABLE {USER} (
+            id integer primary key,
+            name varchar
+        );''')
+        cls.con.execute(f"INSERT INTO {USER} VALUES (?);", (TEST_USER_NAME,))
 
     def tearDown(self) -> None:
         self.con.execute(f'''DELETE FROM {ARTICLES}''')
@@ -21,32 +34,48 @@ class Sqlite3Test(TestCase):
     def tearDownClass(cls) -> None:
         cls.con.close()
 
-    async def write_article(self, title, body):
-        await self.con.execute(f"INSERT INTO {ARTICLES} VALUES (?, ?);", (title, body))
+    async def write_article(self, title: str, body: str, author_id: int):
+        await self.con.execute(f"INSERT INTO {ARTICLES} VALUES (?, ?, ?);", (title, body, author_id))
+
+    async def get_article_by_title(self, title: str) -> Coroutine[Any, Any, Optional[Row]]:
+        cursor = await self.con.execute(f"SELECT * FROM {ARTICLES} WHERE title=:title;", {"title": title})
+        return cursor.fetchone()
+
+    async def test_write_article(self):
+        await self.write_article("제목", "내용", 1)
+        await self.con.commit()
+
+        article = await self.get_article_by_title("제목")
+
+        assert article == (1, "제목", "내용")
 
     async def get_articles(self) -> Coroutine[Any, Any, Iterable[Row]]:
         cursor = await self.con.execute(f"SELECT * FROM {ARTICLES};")
         return cursor.fetchall()
 
-    async def test_write_article(self):
-        await self.write_article("제목", "내용")
-        await self.con.commit()
-
-        article = await self.get_article_by_title("제목")
-
-        assert article == ("제목", "내용")
-
-    async def write_many_articles(self, articles: List[Tuple[str, str]]):
-        await self.con.executemany(f"INSERT INTO {ARTICLES} VALUES (?, ?);", articles)
+    async def write_many_articles(self, articles: List[Tuple[str, str, int]]):
+        await self.con.executemany(f"INSERT INTO {ARTICLES} VALUES (?, ?, ?);", articles)
 
     async def test_write_many_article(self):
-        await self.write_many_articles([("제목", "내용"), ("title", "body")])
+        await self.write_many_articles([("제목", "내용", 1), ("title", "body", 1)])
         await self.con.commit()
 
         articles = await self.get_articles()
 
-        assert articles == [("제목", "내용"), ("title", "body")]
+        assert articles == [(1, "제목", "내용"), (2, "title", "body")]
 
-    async def get_article_by_title(self, title) -> Coroutine[Any, Any, Optional[Row]]:
-        cursor = await self.con.execute(f"SELECT * FROM {ARTICLES} WHERE title=:title;", {"title": title})
+    async def get_article_with_author(self, title: str) -> Coroutine[Any, Any, Iterable[Row]]:
+        cursor = await self.con.execute(f"""
+            SELECT * FROM {ARTICLES}
+            INNER JOIN {USER}
+            ON {ARTICLES}.author={USER}.id
+            WHERE title=:title;""", {"title": title})
         return cursor.fetchone()
+
+    async def test_get_article_with_author(self):
+        await self.write_article("제목", "내용", 1)
+        await self.con.commit()
+
+        article = await self.get_article_with_author("제목")
+
+        assert article == (1, "제목", "내용", TEST_USER_NAME)
--- py-db/sqlalchemy_test.py
+++ py-db/sqlalchemy_test.py
@@ -1,11 +1,22 @@
-from sqlalchemy import create_engine, Column, Integer, String, delete
+from sqlalchemy import create_engine, Column, Integer, String, delete, ForeignKey
 from typing import List
 from unittest import TestCase
-from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, relationship
 
-ARTICLES = "ARTICLES"
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = "USERS"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    my_articles = relationship("Articles", back_populates="author")
+
+    def __repr__(self):
+        return f"Users(id={self.id!r}, name={self.name!r})"
 
 
 class Articles(Base):
@@ -14,9 +25,11 @@ class Articles(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(128), nullable=False)
     body = Column(String(512), nullable=False)
+    author_id = Column(Integer, ForeignKey('USERS.id'))  # 테이블 명
+    author = relationship("Users", back_populates="my_articles")  # 클래스 명
 
     def __repr__(self):
-        return f"Article(id={self.id!r}, title={self.title!r}, body={self.body!r})"
+        return f"Articles(id={self.id!r}, title={self.title!r}, body={self.body!r})"
 
 
 class SqlAlchemyTest(TestCase):
@@ -25,6 +38,8 @@ class SqlAlchemyTest(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         Base.metadata.create_all(engine)
+        cls.user_1 = Users(name="테스트유저", my_articles=[])
+        cls.session.add(cls.user_1)
 
     def tearDown(self) -> None:
         self.session.execute(delete(Articles))
@@ -42,7 +57,7 @@ class SqlAlchemyTest(TestCase):
 
     def test_write_article(self):
         articles = [
-            Articles(title="제목", body="내용")
+            Articles(title="제목", body="내용", author=self.user_1)
         ]
         self.write_articles(articles)
 
@@ -51,8 +66,8 @@ class SqlAlchemyTest(TestCase):
 
     def test_write_many_article(self):
         articles = [
-            Articles(title="제목", body="내용"),
-            Articles(title="title", body="body")
+            Articles(title="제목", body="내용", author=self.user_1),
+            Articles(title="title", body="body", author=self.user_1)
         ]
         self.write_articles(articles)
 
