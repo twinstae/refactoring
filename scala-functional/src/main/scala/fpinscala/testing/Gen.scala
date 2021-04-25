33c855e2c50d069972ef464a46513c73bbbc9360
twinstae <rabolution@gmail.com>
2021-04-25 10:19:23 +0900

    chooseInts 구현

--- scala-functional/src/main/scala/fpinscala/testing/Gen.scala
+++ scala-functional/src/main/scala/fpinscala/testing/Gen.scala
@@ -5,4 +5,26 @@ import fpinscala.state.Reducer
 import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
 
-case class Gen[A](sample: Reducer[RNG, A])
+case class Gen[A](sample: Reducer[RNG, A]) {
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+}
+
+object Gen {
+  // 연습문제 8.4
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(Reducer(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+
+  def chooseInts(size: Int)(start: Int, stopExclusive: Int): Gen[List[Int]] = 
+    listOfN(size, choose(start, stopExclusive))
+
+  // 연습문제 8.5
+  def unit[A](a: => A): Gen[A] =
+    Gen(Reducer.unit(a))
+
+  val boolean: Gen[Boolean] =
+    Gen(Reducer(RNG.boolean))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(Reducer.sequence(List.fill(n)(g.sample)))
+}
