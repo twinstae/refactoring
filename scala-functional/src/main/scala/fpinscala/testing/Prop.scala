47a574cfcd3808e40fe8618ed3a252b7a679ac42
twinstae <rabolution@gmail.com>
2021-04-25 13:50:40 +0900

    스칼라fp Gen, Prop 파일 분리

--- scala-functional/src/main/scala/fpinscala/testing/Prop.scala
+++ scala-functional/src/main/scala/fpinscala/testing/Prop.scala
@@ -1,7 +1,9 @@
 package fpinscala.testing
 
 import Prop._
-import fpinscala.state.Reducer
+import fpinscala.testing.Gen
+import fpinscala.testing.Gen._
+import fpinscala.testing.SGen
 import fpinscala.state.RNG
 
 case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
@@ -61,11 +63,6 @@ object Prop {
     }.find(_.isFalsified).getOrElse(Passed)
     }
   }
-
-  case class Sized[A](forSize: Int => Gen[A]) extends SGen[A]
-  case class Unsized[A](get: Gen[A]) extends SGen[A]
-
-  implicit def unsized[A](g: Gen[A]): SGen[A] = Unsized(g)
 
   def forAll[A](sg: SGen[A])(f: A => Boolean): Prop = sg match {
     case Unsized(g2) => forAll(g2)(f)
@@ -129,57 +126,3 @@ object test_pbt {
     Prop.run(maxProp)
   }
 }
-
-case class Gen[A](sample: Reducer[RNG, A]) {
-  def listOfN(size: Int): Gen[List[A]] =
-    Gen.listOfN(size, this)
-
-  // 연습문제 8.6
-  def flatMap[B](f: A => Gen[B]): Gen[B] =
-    Gen(sample.flatMap(a => f(a).sample))
-
-  def listOfN(size: Gen[Int]): Gen[List[A]] = 
-    size flatMap (n => this.listOfN(n))
-
-  def unsized: SGen[A] =
-    Unsized(this)
-}
-
-object Gen {
-  // 연습문제 8.4
-  def choose(start: Int, stopExclusive: Int): Gen[Int] =
-    Gen(Reducer(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
-
-  val double: Gen[Double] = 
-    Gen(Reducer(RNG.double))
-
-  // 놀이
-  def chooseList(size: Int)(start: Int, stopExclusive: Int): Gen[List[Int]] = 
-    Gen.listOfN(size, Gen.choose(start, stopExclusive))
-
-  // 연습문제 8.5
-  def unit[A](a: => A): Gen[A] =
-    Gen(Reducer.unit(a))
-
-  val boolean: Gen[Boolean] =
-    Gen(Reducer(RNG.boolean))
-
-  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
-    Gen(Reducer.sequence(List.fill(n)(g.sample)))
-
-  // 연습문제 8.7
-  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
-    Gen.boolean flatMap (if (_) g1 else g2)
-
-  // 연습문제 8.8
-  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
-    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
-
-    Gen.double flatMap (d => if(d < threshold) g1._1 else g2._1)
-  }
-
-  def listOf[A](g: Gen[A]): SGen[List[A]] =
-    Sized(size=>listOfN(size, g))
-}
-
-trait SGen[+A]
