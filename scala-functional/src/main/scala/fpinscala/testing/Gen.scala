84dddb7a3dd711aced1947781288ae2c977051c0
twinstae <rabolution@gmail.com>
2021-04-25 10:27:13 +0900

    스칼라fp 연습문제 8.6 flatMap, listOfN(Gen[Int]) 구현

--- scala-functional/src/main/scala/fpinscala/testing/Gen.scala
+++ scala-functional/src/main/scala/fpinscala/testing/Gen.scala
@@ -6,6 +6,9 @@ import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
 
 case class Gen[A](sample: Reducer[RNG, A]) {
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
   def listOfN(size: Int): Gen[List[A]] =
     Gen.listOfN(size, this)
 }
@@ -15,8 +18,9 @@ object Gen {
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
     Gen(Reducer(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
 
+  // 놀이
   def chooseInts(size: Int)(start: Int, stopExclusive: Int): Gen[List[Int]] = 
-    listOfN(size, choose(start, stopExclusive))
+    Gen.listOfN(size, Gen.choose(start, stopExclusive))
 
   // 연습문제 8.5
   def unit[A](a: => A): Gen[A] =
