6921b5599ec15bc6eb41679ed4ce32b84d757214
twinstae <rabolution@gmail.com>
2021-04-25 11:54:58 +0900

    스칼라fp 연습문제 8.9까지 Prop을 Result case로 나눠 상세화

--- scala-functional/src/main/scala/fpinscala/testing/Prop.scala
+++ scala-functional/src/main/scala/fpinscala/testing/Prop.scala
@@ -2,36 +2,73 @@ package testing
 
 import fpinscala.state.RNG
 import Prop._
+import fpinscala.testing.Gen
 
-case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+case class Prop(run: (TestCases, RNG) => Result) {
   def &&(p: Prop) = Prop {
-    (max, n, rng) => run(max, n, rng) match {
-      case Right((a,n)) => p.run(max, n, rng).right.map { case (s,m) => (s, n+m)}
-      case l => l
-    }
-  }
-  def ||(p: Prop) = Prop {
-    (max, n, rng) => run(max, n, rng) match {
-      case Left(msg) => p.tag(msg).run(max, n, rng)
-      case l => l
+    (n, rng) => run(n, rng) match {
+      case Falsified(failure, successes) => Falsified(failure, successes)
+      case Passed => p.run(n, rng) match {
+        case Falsified(failure, successes) => Falsified(failure, n + successes)
+        case Passed => Passed
+      }
     }
   }
 
-  def tag(msg: String) = Prop {
-    (max,n,rng) => run(max,n,rng) match {
-      case Left(e) => Left(msg + "\n" + e)
-      case r => r
+  def ||(p: Prop) = Prop {
+    (n, rng) => run(n, rng) match {
+      case Falsified(failure, successes) =>
+        p.run(n, rng) match {
+          case Falsified(failure2, successes) =>
+            Falsified(failure + "\n" + failure2, n + successes)
+          case Passed => Passed
+        }
+      case Passed => Passed
     }
   }
+
 }
 
 object Prop {
   type TestCases = Int
-  type MaxSize = Int
-  type FailedCase = String
   type SuccessCount = Int
-  type Result = Either[FailedCase, (Status, TestCases)]
+  type FailedCase = String
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    def isFalsified: Boolean = false
+  }
+
+  case class Falsified(
+    failure: FailedCase,
+    successes: SuccessCount
+  ) extends Result {
+    def isFalsified: Boolean = true
+  }
+
+  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(a)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i) 
+      } catch {
+        case e: Exception => Falsified(buildMsg(a, e), i)
+      }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage()}" +
+     "stack trace:\n " + e.getStackTrace.mkString("\n")
 }
+
+
 
 sealed trait Status {}
 
