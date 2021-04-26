21eda60f01888f2951998d1fd1496ce21d75d600
twinstae <rabolution@gmail.com>
2021-04-26 17:35:01 +0900

    hello-ts를 ts-handbook으로 변경, rescript-tutorial 시작

--- rust-lang/src/lib.rs
+++ rust-lang/src/lib.rs
@@ -4,4 +4,21 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn test_generic_lifetime(){
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
+
+        let result = longest(string1.as_str(), string2);
+        assert_eq!(result, string1);
+    }
+    
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
 }
