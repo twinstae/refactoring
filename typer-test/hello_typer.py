84afb7a5cef1ac91db7a9d79eac69f65c321c1ce
twinstae <rabolution@gmail.com>
2021-04-28 18:25:31 +0900

    typer에 docstring 추가

--- typer-test/hello_typer.py
+++ typer-test/hello_typer.py
@@ -4,10 +4,16 @@ app = typer.Typer()
 
 @app.command()
 def hello(name: str):
+    """Hello, {name} 을 출력합니다."""
     typer.echo(f"Hello {name}")
 
 @app.command()
 def goodbye(name: str, formal: bool = False):
+    """
+    Bye, {name}!을 출력합니다.
+
+    --formal 이면 Goodbye Ms. {name}. Have a good day.를 출력합니다.
+    """
     if formal:
         typer.echo(f"Goodbye Ms. {name}. Have a good day.")
     else:
