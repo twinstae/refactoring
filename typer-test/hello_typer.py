44ce9268c96e243224330ecd7efcd8e38f70a10f
twinstae <rabolution@gmail.com>
2021-04-28 18:48:38 +0900

    [typer] argument에 help text 추가

--- typer-test/hello_typer.py
+++ typer-test/hello_typer.py
@@ -8,12 +8,11 @@ def hello(name: str):
     typer.echo(f"Hello {name}")
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    """
-    Bye, {name}!을 출력합니다.
-
-    --formal 이면 Goodbye Ms. {name}. Have a good day.를 출력합니다.
-    """
+def goodbye(
+        name: str,
+        formal: bool = typer.Option(False, help="Goodbye Ms. {name}. Have a good day.를 출력합니다.")
+    ):
+    """Bye, {name}!을 출력합니다."""
     if formal:
         typer.echo(f"Goodbye Ms. {name}. Have a good day.")
     else:
