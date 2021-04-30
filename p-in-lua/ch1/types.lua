e21c87ce2cdd400993a33b209eff633265b50c01
twinstae <rabolution@gmail.com>
2021-04-30 12:17:34 +0900

    프로그래밍 인 루아

---
+++ p-in-lua/ch1/types.lua
@@ -0,0 +1,12 @@
+print("4 and 5")
+print(4 and 5) -- 뒤에 나온 값으로 평가
+print("nil and 13")
+print(nil and 13) -- 앞의 값으로 평가
+print("false and 13")
+print(false and 13)
+print("0 and 4")
+print(0 and 4) -- 뒤
+print("0 or 5")
+print(0 or 5) -- 앞
+print("nil or false")
+print(nil or false) --false
