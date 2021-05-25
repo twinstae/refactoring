e09a8641affce00cd35c27f474b156a5af1fd167
twinstae <rabolution@gmail.com>
2021-05-25 10:05:53 +0900

    Clojure AOC 2019 Day1 part2 까지

--- clojure/noob/src/noob/core.clj
+++ clojure/noob/src/noob/core.clj
@@ -4,4 +4,21 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!"))
+  (println "Hello, World"))
+
+(require '[clojure.edn :as edn])
+(defn parse_mass
+  [input]
+  (edn/read-string input))
+
+(defn calculate_fuel
+  "divide by three, round down, and subtract 2"
+  [mass]
+  (-> mass (/ 3) (Math/floor) (int) (- 2) (max 0)))
+
+(defn calc_fuel_rec
+  "calculate fuel for the mass and then recursively calculate_fuel for the fuel(=new mass)"
+  [acc, mass]
+  (def new_fuel (calculate_fuel mass))
+  (if (> mass 0) (calc_fuel_rec (conj acc new_fuel) new_fuel) acc))
+
--- clojure/noob/test/noob/core_test.clj
+++ clojure/noob/test/noob/core_test.clj
@@ -3,5 +3,18 @@
             [noob.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "I will PASS."
+    (is (= 1 1))))
+
+(deftest calculate_fuel_test
+  (testing "For a mass of 1969, the fuel required is 654"
+    (is (= 654 (calculate_fuel 1969))))
+  (testing "For a mass of 0, the fuel required is 0, not -2"
+    (is (= 0 (calculate_fuel -2)))))
+
+(deftest calc_fuel_rec_test
+  (def result (calc_fuel_rec [] 100756))
+  (testing "결과는 [33583 11192 3728 1240 411 135 43 12 2 0] 이다"
+    (is (= [33583 11192 3728 1240 411 135 43 12 2 0] result)))
+  (testing "총 합은 50346 이다"
+    (is (= 50346 (reduce + result)))))
