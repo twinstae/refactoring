a2401f21e394f6f61ac3514af454475b4c0b685d
twinstae <rabolution@gmail.com>
2021-06-20 15:06:56 +0900

    java-web-nextstep http header line별 읽기

--- java-next-step/web-application-server/src/main/java/webserver/RequestHandler.java
+++ java-next-step/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -4,6 +4,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -22,9 +24,28 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (
+            InputStream in = connection.getInputStream();
+            OutputStream out = connection.getOutputStream()
+        ) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+
+            log.debug("request line : {}", line);
+            String[] tokens = line.split(" ");
+
+            int contentLength = 0;
+            boolean logined = false;
+            while (!line.equals("")) {
+                line = br.readLine();
+                log.debug("header : {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
+
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
