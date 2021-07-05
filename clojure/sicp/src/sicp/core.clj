3e073b26e1974ad2fcae008b9ec6fc6d9dab4dbb
twinstae <rabolution@gmail.com>
2021-07-05 21:49:09 +0900

    sicp ex-1-17까지

--- clojure/sicp/src/sicp/core.clj
+++ clojure/sicp/src/sicp/core.clj
@@ -6,77 +6,64 @@
   [& args]
   (println "Hello, World!"))
 
-(defn a-plus-abs-b
-  [a b]
+(defn a-plus-abs-b [a b]
   ((if (> b 0) + -) a b))
 
-(defn average
-  [x y]
+(defn average [x y]
   (/ (+ x y) 2))
 
-(defn improve
-  [guess x]
+(defn improve [guess x]
   (average guess (/ x guess)))
 
-(defn abs
-  [v]
+(defn abs [v]
   (if (>= v 0) v (- v)))
 
-(defn square
-  [x]
+(defn square [x]
   (* x x))
 
 (defn square-sum [x y] (+ (square x) (square y)))
 
-(defn largest-two-square-sum
-  [x y z]
+(defn largest-two-square-sum [x y z]
   (cond (and (>= x z) (>= y z)) (square-sum x y)
         (and (>= x y) (>= z y)) (square-sum x z)
         :else (square-sum y z)))
 
-(defn good-enough?
-  [guess x]
+(defn good-enough? [guess x]
   (< (abs (- (square guess) x)) 0.001))
 
 (good-enough? 1.41421356237 2)
 
-(defn sqrt-iter
-  [guess x]
+(defn sqrt-iter [guess x]
   (if (good-enough? guess x)
     guess
     (sqrt-iter (improve guess x)
                x)))
 
-(defn sqrt
-  [x]
+(defn sqrt [x]
   (sqrt-iter 1.0 x))
 
 (sqrt 2)
 
-(defn factorial
-  [n]
+(defn factorial [n]
   (if (= n 1)
     1
     (* n (factorial (- n 1)))))
 
 (factorial 3)
 
-(defn fact-iter
-  [product counter max-count]
+(defn fact-iter [product counter max-count]
   (if (> counter max-count)
     product
     (fact-iter (* counter product)
                (+ counter 1)
                max-count)))
 
-(defn factorial2
-  [n]
+(defn factorial2 [n]
   (fact-iter 1 1 n))
 
 (factorial2 3)
 
-(defn A
-  [x y]
+(defn A [x y]
   (cond
     (= y 0) 0
     (= x 0) (* 2 y)
@@ -85,12 +72,10 @@
 
 ;;; test
 
-(defn cube
-  [x]
+(defn cube [x]
   (* x x x))
 
-(defn cube-root
-  [x]
+(defn cube-root [x]
   (defn improve
     [guess]
     (average guess (/ (+ (/ x (square guess))(* 2 guess)) 3)))
@@ -108,8 +93,7 @@
 (cube-root 8)
 (cube-root 2)
 
-(defn first-denomination
-  [kinds-of-coins]
+(defn first-denomination [kinds-of-coins]
   (cond
     (= kinds-of-coins 1) 1 
     (= kinds-of-coins 2) 5
@@ -117,8 +101,7 @@
     (= kinds-of-coins 4) 25
     (= kinds-of-coins 5) 50))
 
-(defn cc
-  [amount kinds-of-coins]
+(defn cc [amount kinds-of-coins]
   (cond
     (= amount 0) 1
     (or (< amount 0) (= kinds-of-coins 0)) 0
@@ -127,12 +110,10 @@
              (cc (- amount (first-denomination kinds-of-coins))
                  kinds-of-coins))))
 
-(defn count-change
-  [amount]
+(defn count-change [amount]
   (cc amount 5))
 
-(defn f-iter
-  [[a b c] now-n target-n]
+(defn f-iter [[a b c] now-n target-n]
   (cond
     (= now-n target-n) c
     :else
@@ -141,8 +122,26 @@
         (+ now-n 1)
         target-n)))
 
-(defn f
-  [n]
+(defn f [n]
   (cond
     (< n 3) n
     :else (f-iter [1 2 4] 3 n)))
+
+(defn next-line [above]
+  (map + (concat above [0]) (concat [0] above)))
+
+(defn pascal [n]
+  (if (= n 1) [1] (next-line (pascal (- n 1)))))
+
+(defn fast-expt [b n]
+  (cond (= n 0) 1
+        (even? n) (square (fast-expt b (/ n 2)))
+        :else (* b (fast-expt b (- n 1)))))
+
+(defn double-by-plus [n]
+  (+ n n))
+
+(defn multiply-int [a b]
+  (cond (= b 1) a
+        (even? b) (double-by-plus (multiply-int a (/ b 2)))
+        :else (+ a (multiply-int a (- b 1)))))
--- clojure/sicp/test/sicp/core_test.clj
+++ clojure/sicp/test/sicp/core_test.clj
@@ -36,9 +36,28 @@
     (is (= (count-change 10) 4))
     (is (= (count-change 100) 292))))
 
-(deftest Ex-1.11
+(deftest Ex-1-11
   (testing "f 함수"
     (is (= (f 2) 2))
     (is (= (f 3) 4))
     (is (= (f 4) 11))
     (is (= (f 5) 25))))
+
+(deftest Ex-1-12
+  (testing "pascal"
+    (is (= (pascal 1) [1]))
+    (is (= (pascal 2) [1 1]))
+    (is (= (pascal 3) [1 2 1]))
+    (is (= (pascal 4) [1 3 3 1]))))
+
+(deftest Ex-fast-expt
+  (testing "fast-expt"
+    (is (= (fast-expt 2 4) 16))
+    (is (= (fast-expt 2 5) 32))
+    (is (= (fast-expt 2 10) 1024))
+    (is (= (fast-expt 2 11) 2048))))
+
+(deftest Ex-1-17
+  (testing "multiply-int"
+    (is (= (multiply-int 4 4) (* 4 4)))
+    (is (= (multiply-int 16 15) (* 16 15)))))
