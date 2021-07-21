4d6e3880759db8bb1ad3ea55a9f0f01aed448467
twinstae <rabolution@gmail.com>
2021-07-21 09:23:25 +0900

    learn c the hard way 10번까지

---
+++ lchw/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 이거 코멘트임. */
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  // 이것도 코멘트임
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
---
+++ lchw/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int age = 26;
+  int height = 167;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d cm tall.\n", height);
+
+  return 0;
+}
